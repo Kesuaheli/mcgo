2bb119287b3b9aacb4dfd2fd47df489cd505200b
Kesuaheli <kesuaheligame@gmail.com>
2025-09-29 00:38:52 +0200

    implementing chunk data encoding
    Co-Authored-By: Buh <buh@mintendo-programmer.de>

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"mcgo/world"
 	"net"
 )
 
@@ -10,9 +11,14 @@ type Server struct {
 	clients  []*Client
 }
 
+var World *world.World = world.NewWorld()
+
 // StartServer starts a new server listening to the given port number. If port is 0, an available
 // port number is automatically chosen.
 func StartServer(port int) (server *Server, err error) {
+	World.GetChunk(0, 0).GetChunkSection(0).FillWithBlocks(0, 0, 0, 15, 0, 15, "minecraft:cobblestone")
+	World.GetChunk(0, 0).GetChunkSection(0).FillWithBlocks(0, 1, 0, 15, 2, 15, "minecraft:dirt")
+	World.GetChunk(0, 0).GetChunkSection(0).FillWithBlocks(0, 3, 0, 15, 3, 15, "minecraft:grass_block")
 	server = &Server{}
 	server.listener, err = net.ListenTCP("tcp", &net.TCPAddr{Port: port})
 	if err != nil {
--- world/chunkData.go
+++ world/chunkData.go
@@ -9,7 +9,29 @@ import (
 	"mcgo/types"
 )
 
+type XZPosition struct {
+	x int32
+	z int32
+}
+type World struct {
+	chunks map[XZPosition]*Chunk
+}
+
+type Chunk struct {
+	dimensionHeight int
+
+	chunkSections []*ChunkSection
+}
+
 type ChunkSection struct {
+	blocks [16][16][16]*Block
+}
+
+type Block struct {
+	id     string
+	states map[string]any
+}
+type ChunkSectionFormat struct {
 	BlockCount   int16
 	BitsPerBlock uint8
 	Data         []uint64
@@ -192,4 +214,139 @@ func (container PalettedContainerType) GetFormat(bitsPerEntry *uint8) PaletteFor
 	default:
 		panic("Unknown container type")
 	}
+}
+
+// convert internal to ChunkSectionNetProtocol
+func (chunksection *ChunkSection) ToBytesOnWire() []byte {
+
+	blockCount := uint16(0)
+	// analyse data for paletted container
+
+	bufBlockData := &bytes.Buffer{}
+	blockIdToRegistryId := map[string]uint16{
+		"minecraft:air":         0,
+		"minecraft:stone":       1,
+		"minecraft:grass_block": 9,
+		"minecraft:dirt":        10,
+		"minecraft:cobblestone": 14,
+	}
+
+	for y := range 16 {
+		for z := range 16 {
+			for x := range 4 {
+				data := uint64(0)
+				for i := range 4 {
+					data <<= 15
+					block := chunksection.blocks[y][z][x*4+i]
+					if block.id != "minecraft:air" && block.id != "minecraft:void_air" && block.id != "minecraft:cave_air" {
+						blockCount++
+					}
+					data |= uint64(blockIdToRegistryId[block.id])
+				}
+				binary.Write(bufBlockData, binary.BigEndian, data)
+			}
+		}
+	}
+
+	buf := &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, blockCount)
+	binary.Write(buf, binary.BigEndian, uint8(15))
+	// no palete
+	// block data
+	buf.Write(bufBlockData.Bytes())
+
+	// biome data
+	// single value
+	binary.Write(buf, binary.BigEndian, uint8(0))
+	// value is set as VarInt. 57 -> minecraft:the_void
+	types.WriteVarInt(buf, 57)
+
+	return buf.Bytes()
+
+}
+
+func NewChunkSection() *ChunkSection {
+	chunkSection := ChunkSection{}
+	for y := range 16 {
+		for z := range 16 {
+			for x := range 16 {
+				chunkSection.blocks[y][z][x] = &Block{id: "minecraft:air"}
+			}
+		}
+	}
+
+	return &chunkSection
+}
+
+func (chunksection *ChunkSection) SetBlock(x, y, z int, id string) {
+	chunksection.blocks[y][z][x].id = id
+	chunksection.blocks[y][z][x].states = nil
+}
+
+func (chunksection *ChunkSection) FillWithBlocks(xMin, yMin, zMin, xMax, yMax, zMax int, id string) {
+	for yOffset := range (yMax + 1) - yMin {
+		y := yOffset + yMin
+		for zOffset := range (zMax + 1) - zMin {
+			z := zOffset + zMin
+			for xOffset := range (xMax + 1) - xMin {
+				x := xOffset + xMin
+				chunksection.blocks[y][z][x].id = id
+				chunksection.blocks[y][z][x].states = nil
+			}
+		}
+	}
+}
+
+func NewChunk() (chunk *Chunk) {
+	chunk = &Chunk{
+		dimensionHeight: 24,
+	}
+	for range chunk.dimensionHeight {
+		chunk.chunkSections = append(chunk.chunkSections, NewChunkSection())
+	}
+	return chunk
+}
+
+func (chunk *Chunk) GetChunkSection(index uint) *ChunkSection {
+	return chunk.chunkSections[index]
+}
+
+func NewWorld() (world *World) {
+	world = &World{
+		chunks: map[XZPosition]*Chunk{},
+	}
+
+	worldSize := int32(5)
+	worldSizeInDirection := worldSize / 2
+	for x := -worldSizeInDirection; x <= worldSizeInDirection; x++ {
+		for z := -worldSizeInDirection; z <= worldSizeInDirection; z++ {
+			world.chunks[XZPosition{x, z}] = NewChunk()
+		}
+	}
+
+	return world
+}
+
+func (world *World) GetChunk(x, z int32) (chunk *Chunk) {
+	return world.chunks[XZPosition{x, z}]
+}
+
+func (chunk *Chunk) ToChunkData() []byte {
+	buf := &bytes.Buffer{}
+
+	// heightmaps
+	buf.WriteByte(0)
+
+	// data
+	chunksectionBuf := &bytes.Buffer{}
+	for _, chunksection := range chunk.chunkSections {
+		chunksectionBuf.Write(chunksection.ToBytesOnWire())
+	}
+	types.WriteVarInt(buf, int32(chunksectionBuf.Len()))
+	buf.Write(chunksectionBuf.Bytes())
+
+	// block entities
+	buf.WriteByte(0)
+
+	return buf.Bytes()
 }
